74423ebb37853d9a237381f67fc3408cd889054d
Eldorbek Makhkamov <52928263+makhweb@users.noreply.github.com>
2020-02-29 12:50:32 +0500

    BladeX initalization fixed ✔

--- resources/views/assets.blade.php
+++ resources/views/assets.blade.php
@@ -3,8 +3,8 @@
 
 <script>
   const bladeXLoader = new BladeX({
-  	rootID: {{ config('blade-x-loader.id') }},
-  	tagAttr: {{ config('blade-x-loader.tag_attribute') }}
+  	rootID: '{{ config('blade-x-loader.id') }}',
+  	tagAttr: '{{ config('blade-x-loader.tag_attribute') }}'
   })
 
   bladeXLoader.init()
