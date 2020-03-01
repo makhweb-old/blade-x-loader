3898ea24993d3de0c03b494fc03a6ec44000f820
Eldorbek Makhkamov <52928263+makhweb@users.noreply.github.com>
2020-03-01 19:44:26 +0500

    New directive added ✔

--- src/BladeXLoaderServiceProvider.php
+++ src/BladeXLoaderServiceProvider.php
@@ -4,6 +4,9 @@ namespace Makhweb\BladeXLoader;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 class BladeXLoaderServiceProvider extends ServiceProvider
 {
@@ -17,6 +20,7 @@ class BladeXLoaderServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__ . '/../config/BladeXLoader.php', 'blade-x-loader');
         $this->publishThings();
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'blade-x-loader');
+        $this->addDirections()
         // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         // $this->registerRoutes();
     }
@@ -40,5 +44,35 @@ class BladeXLoaderServiceProvider extends ServiceProvider
                 __DIR__.'/../publishable' => public_path('vendor/makhweb/blade-x-loader'),
             ], 'publishable');
         }
+    }
+
+    /**
+     * Get additions of Laravel Blade
+     */
+    private function bladeAdittions() : Collection
+    {
+        return new Collection([
+            [
+                'name' => 'ajax',
+                'type' => 'if',
+                'callback' => function () {
+                    return Request::isAjax();
+                }
+            ]
+        ]);
+    }
+
+    /**
+     * Add directions to Laravel Blade
+     */
+    private function addDirections()
+    {
+        $this->bladeAdittions()->each(function($direction){
+            call_user_func(
+                [Blade::class, $direction->type],
+                $direction->name,
+                $direction->callback
+            )
+        })
     }
 }
