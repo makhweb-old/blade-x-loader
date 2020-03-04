83c3a41553a0bcd0569e4739344de083d4eb0ced
Eldorbek Makhkamov <52928263+makhweb@users.noreply.github.com>
2020-03-04 12:09:50 +0500

    Fixes #4 ✔

--- resources/views/assets.blade.php
+++ resources/views/assets.blade.php
@@ -2,7 +2,7 @@
 <script src="{{ asset('vendor/makhweb/blade-x-loader/js/app.js') }}"></script>
 
 <script>
-  const bladeXLoader = new BladeX({
+  const bladeXLoader = new BladeXLoader({
   	rootID: '{{ config('blade-x-loader.id') }}',
   	tagAttr: '{{ config('blade-x-loader.tag_attribute') }}'
   })
